f6a363d689bd55b8a013562eda89ff6fb9731e82
Johan Manuel Gonzalez <gonzalezjoan@hotmail.com>
2019-11-25 23:29:56 +0100

    declaración de variables, creacion de metodo numerico()

--- BasicosJohanManuel/src/basicosjohanmanuel/BasicosJohanManuel.java
+++ BasicosJohanManuel/src/basicosjohanmanuel/BasicosJohanManuel.java
@@ -16,6 +16,31 @@ public class BasicosJohanManuel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        numericos();  // Llamada al método
+    }
+    
+    /**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.print("Valor entero ");
+        System.out.println(entero);
+        System.out.print("Valor doble ");
+        System.out.println(doble);
     }
     
 }
