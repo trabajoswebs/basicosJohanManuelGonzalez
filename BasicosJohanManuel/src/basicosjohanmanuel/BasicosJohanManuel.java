1039e031394fb7d48da2fc132d338b550c359b8d
Johan Manuel Gonzalez <gonzalezjoan@hotmail.com>
2019-11-25 23:53:41 +0100

    Declaro variables y creo el metodo logicos()

--- BasicosJohanManuel/src/basicosjohanmanuel/BasicosJohanManuel.java
+++ BasicosJohanManuel/src/basicosjohanmanuel/BasicosJohanManuel.java
@@ -16,6 +16,26 @@ public class BasicosJohanManuel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        logicos();
+    }
+    
+    public static void logicos() {
+
+        //Estos son los tipos básicos
+        int random;
+        int edad;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        random = (int) (Math.random() * (45 - 5 + 1) + 5) ;
+        edad = 18;
+        
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        if(random >= 18){
+            System.out.println("Ya tienes edad para aprender a conducir.");
+        }
+        else{
+            System.out.println("Eres muy joven para poder conducir");
+        }
     }
     
 }
