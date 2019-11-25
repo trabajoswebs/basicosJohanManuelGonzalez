f4534852202818a8b8656a298e4a7a287b33392a
Johan Manuel Gonzalez <gonzalezjoan@hotmail.com>
2019-11-25 23:41:02 +0100

    Declaro variables y creo el metodo cadenas()

--- BasicosJohanManuel/src/basicosjohanmanuel/BasicosJohanManuel.java
+++ BasicosJohanManuel/src/basicosjohanmanuel/BasicosJohanManuel.java
@@ -16,6 +16,29 @@ public class BasicosJohanManuel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        cadenas();  // Llamada al método
     }
+    
+    /**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void cadenas() {
+
+        //Estos son los tipos básicos
+        String nombre;
+        String apellidos;
+        
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        nombre = "Johan Manuel";
+        apellidos = "GONZÁLEZ RODRÍGUEZ";
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.print("El nombre del alumno es: ");
+        System.out.println(nombre);
+        System.out.print("El apellido es: ");
+        System.out.println(apellidos);
+    }
+    
     
 }
